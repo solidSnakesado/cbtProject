e6d79f79ffa8c090f41905695919c69232f69931
User <User@YD-PC-3-14>
2019-06-26 17:28:33 +0900

    컴퍼니 컨트롤러 테스트

--- cbtProject/src/main/java/com/cbt/company/CompanyController.java
+++ cbtProject/src/main/java/com/cbt/company/CompanyController.java
@@ -1,8 +1,12 @@
 package com.cbt.company;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CompanyController {
-
+	@RequestMapping("companyMain.do")
+	public String companyMain() {
+		return "company/companyMain";
+	}
 }
