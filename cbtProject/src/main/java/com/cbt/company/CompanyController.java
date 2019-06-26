e79d124b91de5f52fb77ee74c16fff87d953649c
solidSnakesado <52153542+solidSnakesado@users.noreply.github.com>
2019-06-26 16:47:09 +0900

    구조 변경

---
+++ cbtProject/src/main/java/com/cbt/company/CompanyController.java
@@ -0,0 +1,8 @@
+package com.cbt.company;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CompanyController {
+
+}
---
+++ cbtProject/src/main/java/com/cbt/manager/ManagerController.java
@@ -0,0 +1,8 @@
+package com.cbt.manager;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ManagerController {
+
+}
