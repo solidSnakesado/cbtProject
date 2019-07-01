86f11b83c35e54a3cc6bc9f924a08b80342dccac
User <User@YD-PC-3-14>
2019-07-01 18:06:48 +0900

    candidateController-> candidate2Controller

---
+++ cbtProject/src/main/java/com/cbt/candidate/Candidate2Controller.java
@@ -0,0 +1,34 @@
+package com.cbt.candidate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//2019.07.01 재홍- candidatecontroller -> candidate2controller
+@Controller
+public class Candidate2Controller {
+
+	@RequestMapping("candidateInHeader.do")
+	public String candidateInHeader() {
+		return "candidate/candidateInHeader";
+	}
+
+	@RequestMapping("candidateInIntroduce.do")
+	public String candidateInIntroduce() {
+		return "candidate/candidateInIntroduce";
+	}
+	
+	@RequestMapping("candidatePersonalChart.do")	
+	public String candidatePersonalChart() {
+		return "candidate/candidatePersonalChart";
+	}
+	
+	@RequestMapping("candidateScheduleCheck.do")	
+	public String candidateScheduleCheck() {
+		return "candidate/candidateScheduleCheck";
+	}
+	
+	@RequestMapping("candidateSurvey.do")	
+	public String candidateSurvey() {
+		return "candidate/candidateSurvey";
+	}
+}
