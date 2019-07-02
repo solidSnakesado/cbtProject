fd3710c16afb1f697fef669549ca3de258c4939e
user <user@user-PC3006>
2019-07-02 11:33:00 +0900

    컴퍼니 입력 처리 추가

--- cbtProject/src/main/java/com/cbt/company/CompanyController.java
+++ cbtProject/src/main/java/com/cbt/company/CompanyController.java
@@ -1,19 +1,30 @@
 package com.cbt.company;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // 2019.06.27 성재민
 // 컴퍼니 메인 컨트롤러 연결
 @Controller
 public class CompanyController {
+	@Autowired
+	CompanyService companyService;
+	
 	@RequestMapping("companyMain.do")
 	public String companyMain() {
 		return "company/companyMain";
 	}
 	
-	@RequestMapping("companySignUp.do")
-	public String companySignUp() {
+	@RequestMapping(value = "companySignUp.do", method = RequestMethod.GET)
+	public String companySignUpForm() {
+		return "company/companySignUp";
+	}
+	
+	@RequestMapping(value = "companySignUp.do", method = RequestMethod.POST)
+	public String companySignUp(CompanyVO vo) {
+		companyService.insertCompany(vo);
 		return "company/companySignUp";
 	}
 	
