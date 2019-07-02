04742fbcae36c17d675ac3f61bc5da31068491a4
Root <Root@DESKTOP-5V5HKON>
2019-07-02 10:07:45 +0900

    test

---
+++ cbtProject/src/main/java/com/cbt/candidate/Candidate4Controller.java
@@ -0,0 +1,15 @@
+package com.cbt.candidate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Candidate4Controller {
+	
+//	@RequestMapping("candidateSurvey.do")	
+//	public String candidateSurvey() {
+//		return "candidate/candidateSurvey";
+//	}
+	
+}
