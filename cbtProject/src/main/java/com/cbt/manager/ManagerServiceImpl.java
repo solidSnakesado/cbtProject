c2fd4a5fc8da9c01d0715bac3ed544589bcd4f60
User <User@YD-PC-3-016>
2019-07-03 14:42:40 +0900

    restcontroller 삭제

--- cbtProject/src/main/java/com/cbt/manager/ManagerServiceImpl.java
+++ cbtProject/src/main/java/com/cbt/manager/ManagerServiceImpl.java
@@ -5,8 +5,11 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import com.cbt.common.Paging;
 
+@Service("managerService")
 public class ManagerServiceImpl implements ManagerService {
 	
 	@Resource
