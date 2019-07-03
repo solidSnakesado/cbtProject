e196c38c99fa7745dd8bef14e7a19e6cb908722c
User <User@YD-PC-3-14>
2019-07-03 18:24:10 +0900

    category vo,dao,mapper수정
    estimate
    vo,dao,mapper수정
    
    exam
    vo,dao,mapper수정
    
    survey
    dao,mapper수정

--- cbtProject/src/main/java/com/cbt/category/CategoryVO.java
+++ cbtProject/src/main/java/com/cbt/category/CategoryVO.java
@@ -1,41 +1,58 @@
 package com.cbt.category;
 
 public class CategoryVO {
-
-	private String mainCategory;
-	private String middleCategory;
-	private String smallCategory;
-	private int categoryId;
+	/*19 07 03 재홍 DB맞게 수정*/
+	private String categoryId;
+	private int mainCategory;
+	private int middleCategory;
+	private int smallCategory;
 	
-	public String getMainCategory() {
-		return mainCategory;
-	}
-	public void setMainCategory(String mainCategory) {
-		this.mainCategory = mainCategory;
-	}
-	public String getMiddleCategory() {
-		return middleCategory;
-	}
-	public void setMiddleCategory(String middleCategory) {
-		this.middleCategory = middleCategory;
-	}
-	public String getSmallCategory() {
-		return smallCategory;
-	}
-	public void setSmallCategory(String smallCategory) {
-		this.smallCategory = smallCategory;
-	}
-	public int getCategoryId() {
+	
+	public String getCategoryId() {
 		return categoryId;
 	}
-	public void setCategoryId(int categoryId) {
+
+
+	public void setCategoryId(String categoryId) {
 		this.categoryId = categoryId;
 	}
-	
+
+
+	public int getMainCategory() {
+		return mainCategory;
+	}
+
+
+	public void setMainCategory(int mainCategory) {
+		this.mainCategory = mainCategory;
+	}
+
+
+	public int getMiddleCategory() {
+		return middleCategory;
+	}
+
+
+	public void setMiddleCategory(int middleCategory) {
+		this.middleCategory = middleCategory;
+	}
+
+
+	public int getSmallCategory() {
+		return smallCategory;
+	}
+
+
+	public void setSmallCategory(int smallCategory) {
+		this.smallCategory = smallCategory;
+	}
+
+
 	@Override
 	public String toString() {
 		return "CategoryVO [mainCategory=" + mainCategory + ", middleCategory=" + middleCategory + ", smallCategory="
 				+ smallCategory + ", categoryId=" + categoryId + "]";
 	}
+	
 	
 }
--- cbtProject/src/main/java/com/cbt/survey/SurveyDAO.java
+++ cbtProject/src/main/java/com/cbt/survey/SurveyDAO.java
@@ -6,28 +6,28 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class SurveyDAO {
-	
+	//190703  재홍  mapper와 연결
 	@Autowired
 	private SqlSessionTemplate mybatis;
 	
 	public void insertSurvey(SurveyVO vo) {
 		
-		mybatis.insert("", vo);
+		mybatis.insert("SurveyDAO.insertSurvey", vo);
 	}
 	
 	public void updateSurvey(SurveyVO vo) {
 		
-		mybatis.update("", vo);
+		mybatis.update("SurveyDAO.updateSurvey", vo);
 	}
 
 	public void deleteSurvey(SurveyVO vo) {
 		
-		mybatis.delete("", vo);
+		mybatis.delete("SurveyDAO.deleteSurvey", vo);
 	}
 	
 	public SurveyVO getSurvey(SurveyVO vo) {
 		
-		return mybatis.selectOne("", vo);
+		return mybatis.selectOne("SurveyDAO.getSurveyID", vo);
 	}
 	
 }
