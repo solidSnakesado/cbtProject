29d3f04ae572afe13b45803f3959d2ce35f85b0d
User <User@User-PC>
2019-07-12 13:58:56 +0900

    시험일정 뷰 보기 수정

--- cbtProject/src/main/java/com/cbt/candidate/CandidateController.java
+++ cbtProject/src/main/java/com/cbt/candidate/CandidateController.java
@@ -1,7 +1,6 @@
 package com.cbt.candidate;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -19,9 +18,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cbt.common.Paging;
-import com.cbt.exam.*;
+import com.cbt.exam.ExamService;
+import com.cbt.exam.ExamVO;
 import com.cbt.takeExam.TakeExamService;
-import com.cbt.takeExam.TakeExamVO;
 
 
 //2019.07.01 장세준 - candidate3controller로 이전
@@ -204,18 +203,31 @@ public class CandidateController {
 		return "candidate/candidate/candidatePersonalChart";
 	}
 	
-	/*
-	 * //temp
-	 * @RequestMapping(value="candidateScheduleCheck.do", method =
-	 * RequestMethod.GET) public String candidateScheduleCheck() { return
-	 * "candidate/candidate/candidateScheduleCheck"; }
-	 */
+	// 시험일정 전체 보기(로그인 없이 보기)
+	@RequestMapping(value = "candidateScheduleCheck.do", method = RequestMethod.GET)
+	public ModelAndView candidateScheduleCheck() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("candidateScheduleCheck", candidateService.candidateScheduleCheck());
+		mv.setViewName("candidate/candidate/candidateScheduleCheck");
+			
+		return mv;
+	}
 	
-
+	// 시험일정 가져오기
+//	@RequestMapping(value = "candidateScheduleCheck.do", method = RequestMethod.GET)
+//	public String candidateScheduleCheck(HttpSession session, Model model, Paging paging) {
+//		CandidateVO loginCandidate = (CandidateVO) session.getAttribute("candidate");
+//		if(loginCandidate != null) {
+//			CandidateVO vo = new CandidateVO();
+//			vo.setTakerId(loginCandidate.getTakerId());
+//			model.addAttribute("result",candidateService.ExamSchedule(vo, paging));
+//			return "candidate/candidate/candidateScheduleCheck";
+//		} else {
+//			return "candidate/candidate/candidateLogin";
+//		}
+//	}
 	
-	
-	
-	
+		
 	
 	// 2019.07.10 장세준
 	@ResponseBody
--- cbtProject/src/main/java/com/cbt/takeExam/TakeExamController.java
+++ cbtProject/src/main/java/com/cbt/takeExam/TakeExamController.java
@@ -16,16 +16,15 @@ public class TakeExamController {
 	@Autowired
 	TakeExamService takeExamService;
 
-	@RequestMapping(value="candidateScheduleCheck.do", method = RequestMethod.GET)
-	public String candidateScheduleCheck(HttpSession session, Model model, Paging paging) {
-		CandidateVO loginCandidate = (CandidateVO) session.getAttribute("candidate");
-		if(loginCandidate != null) {
-			TakeExamVO vo = new TakeExamVO();
-			vo.setTakerId(loginCandidate.getTakerId());
-			model.addAttribute("result",takeExamService.getExamList(vo, paging));
-			return "candidate/candidate/candidateScheduleCheck";
-		} else {
-		return "candidate/candidate/candidateLogin";
-		}
-	}
+	// 7.11 생성, 준 -> 응시자가 시험일정에서 내역 보기
+	/*
+	 * @RequestMapping(value="candidateScheduleCheck.do", method =
+	 * RequestMethod.GET) public String candidateScheduleCheck(HttpSession session,
+	 * Model model, Paging paging) { CandidateVO loginCandidate = (CandidateVO)
+	 * session.getAttribute("candidate"); if(loginCandidate != null) { TakeExamVO vo
+	 * = new TakeExamVO(); vo.setTakerId(loginCandidate.getTakerId());
+	 * model.addAttribute("result",takeExamService.getExamList(vo, paging)); return
+	 * "candidate/candidate/candidateScheduleCheck"; } else { return
+	 * "candidate/candidate/candidateLogin"; } }
+	 */
 }
