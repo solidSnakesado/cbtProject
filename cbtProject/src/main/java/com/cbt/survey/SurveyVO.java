92e805c20be84ffdb3ed2b89c38184cfd967c172
User <User@User-PC>
2019-07-17 18:21:14 +0900

    암호화 주석처리(테스트용도)/surveyVO에survey_questionVO추가중

--- cbtProject/src/main/java/com/cbt/survey/SurveyVO.java
+++ cbtProject/src/main/java/com/cbt/survey/SurveyVO.java
@@ -6,12 +6,42 @@ public class SurveyVO {
 	private int surveyType;
 	private int takeExamId;
 	
+	// 설문조사 문제
+	private int surveyQuestionNumber;
+	private int contents;
+	private int value;
+	
+	
 	public int getSurveyId() {
 		return surveyId;
 	}
 
 	public void setSurveyId(int surveyId) {
 		this.surveyId = surveyId;
+	}
+
+	public int getSurveyQuestionNumber() {
+		return surveyQuestionNumber;
+	}
+
+	public void setSurveyQuestionNumber(int surveyQuestionNumber) {
+		this.surveyQuestionNumber = surveyQuestionNumber;
+	}
+
+	public int getContents() {
+		return contents;
+	}
+
+	public void setContents(int contents) {
+		this.contents = contents;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
 	}
 
 	public int getSurveyType() {
